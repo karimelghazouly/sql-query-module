d4adadb0485e2fe8752b66470836aef0fe2f3b89
Mohamed Essam Arafa <mohamed.essam.arafa@gmail.com>
2018-04-13 17:06:34 +0200

    Skeleton Code

---
+++ QueryModule/QueryParser/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryModule.QueryParser
+{
+    enum NodeType
+    {
+
+    }
+    class Node
+    {
+        NodeType nodeType;
+        List<Node> Children;
+    }
+    class ParserResult
+    {
+        List<Node> selectNodes;
+        Node fromNode;
+        Node whereNode;
+    }
+    class Parser
+    {
+        public static ParserResult parse(List<Token> tokens)
+        {
+            return null;
+        }
+    }
+}
---
+++ QueryModule/QueryParser/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryModule.QueryParser
+{
+    enum TokenType
+    {
+        SELECT, OP, ID, NUM, FROM, WHERE, L_PARA, R_PARA, COMMA, AND, OR
+    }
+    class Token
+    {
+        TokenType tokenType;
+        string lexeme;
+    }
+}
