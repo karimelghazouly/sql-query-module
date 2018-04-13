9758fb26b0d8b2e44821c4974777a8cd507ad84d
Mohamed Essam Arafa <mohamed.essam.arafa@gmail.com>
2018-04-13 17:06:34 +0200

    Skeleton Code

---
+++ QueryModule/QueryParser/Lexer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryModule.QueryParser
+{
+    class Lexer
+    {
+        public static List<Token> Lex(string query)
+        {
+            return null;
+        }
+    }
+}
