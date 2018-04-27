a6f564ad8ef3ca144ca39410a425dc547f58fc42
Mahmoud Hamdy <mahmoudhy97@hotmail.com>
2018-04-27 15:01:56 +0200

    UI done

---
+++ QueryModule/QueryModuleForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace QueryModule
+{
+    partial class QueryModuleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QueryModuleForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(550, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(569, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 181);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(625, 304);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(247, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search Query";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(13, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Select file to search in";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // QueryModuleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(650, 498);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "QueryModuleForm";
+            this.Text = "Query Module";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
--- QueryModule/QueryModuleForm.cs
+++ QueryModule/QueryModuleForm.cs
@@ -11,25 +11,28 @@ using QueryModule.QueryParser;
 
 namespace QueryModule
 {
-    public partial class Form1 : Form
+    public partial class QueryModuleForm : Form
     {
-        public Form1()
+        public QueryModuleForm()
         {
             InitializeComponent();
+            dataGridView1.Visible = false;
+            label1.BackColor = Color.Transparent;
+            button1.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             string text = textBox1.Text.Trim();
-            if(text == "")
+            if (text == "")
             {
                 MessageBox.Show("Please write your SQL query.");
                 return;
             }
             List<Token> tokens = lexer.Lex(text);
             ParserResult node = Parser.parse(tokens);
-            List < List < string > > data = new List<List<string>>();
-            for(int i=0;i<3;i++)
+            List<List<string>> data = new List<List<string>>();
+            for (int i = 0; i < 3; i++)
             {
                 List<string> tt = new List<string>();
                 for (int j = 0; j < 3; j++)
@@ -38,7 +41,7 @@ namespace QueryModule
             }
             List<string> cols = new List<string>();
             string name = "";
-            for(int i=0;i<tokens.Count;i++)
+            for (int i = 0; i < tokens.Count; i++)
             {
                 if (tokens[i].tokenType == TokenType.SELECT)
                     continue;
@@ -54,17 +57,28 @@ namespace QueryModule
                 }
                 else
                 {
-                    name += tokens[i].lexeme+" ";
+                    name += tokens[i].lexeme + " ";
                 }
             }
             DataTable table = new DataTable();
             for (int i = 0; i < cols.Count; i++)
                 table.Columns.Add(cols[i]);
-            for(int i=0;i<data.Count;i++)
+            for (int i = 0; i < data.Count; i++)
             {
-                table.Rows.Add(data[i].ToArray());     
+                table.Rows.Add(data[i].ToArray());
             }
             dataGridView1.DataSource = table;
+            dataGridView1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var FD = new System.Windows.Forms.OpenFileDialog();
+            if (FD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToOpen = FD.FileName;
+                button1.Enabled = true;
+            }
         }
     }
 }
