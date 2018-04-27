e075756725f99b4001b0ec223a35dc126a8f1c28
Karim El Ghazouly <karimelghazouly@gmail.com>
2018-04-27 10:25:29 +0200

    Tokenizer

--- QueryModule/QueryParser/Lexer.cs
+++ QueryModule/QueryParser/Lexer.cs
@@ -6,11 +6,158 @@ using System.Threading.Tasks;
 
 namespace QueryModule.QueryParser
 {
-    class Lexer
+    class lexer
     {
-        public static List<Token> Lex(string query)
+        public lexer()
+        { }
+        bool isoperator(char x)
         {
-            return null;
+            return (x == '+' || x == '-' || x == '*' || x == '/' || x == '>' || x == '<' || x == '=' || x == '!'
+                || x == '%');
         }
+        public List<Token> Lex(string query)
+        {
+            query = query.Trim();
+            int idx = -1;
+            List<Token> ret = new List<Token>();
+            string select_check = query.Substring(0, 6);
+            select_check = select_check.ToLower();
+            if (select_check != "select")
+            {
+                // raise exepction
+                Console.WriteLine("awl btngan");
+                return null;
+            }
+            Token n = new Token("select", 0);
+            ret.Add(n);
+            string temp = "";
+            for (int i = 6; i < query.Length; i++)
+            {
+                if (i + 3 < query.Length && query.Substring(i, 4).ToLower() == "from")
+                {
+                    idx = i + 4;
+                    ret.Add(new Token("from", TokenType.FROM));
+                    break;
+                }
+                if (query[i] == ' ' || query[i] == ',' || isoperator(query[i]) || query[i] == '(' || query[i] == ')')
+                {
+                    if (temp.Count() != 0)
+                    {
+                        TokenType t = new Token().get_type(temp);
+                        if (t == TokenType.EXCEPT)
+                        {
+                            //raise btnagan
+                            Console.WriteLine("tny btngan");
+                            return null;
+                        }
+                        ret.Add(new Token(temp, t));
+                    }
+
+                    if (query[i] == '(')
+                        ret.Add(new Token("(", TokenType.L_PARA));
+                    else if (query[i] == ')')
+                        ret.Add(new Token(")", TokenType.R_PARA));
+
+                    else if (isoperator(query[i]))
+                    {
+                        string o = query[i].ToString();
+                        if (isoperator(query[i + 1]))
+                        {
+                            o += query[i + 1];
+                            i++;
+                        }
+                        ret.Add(new Token(o, TokenType.OP));
+                    }
+                    else if (query[i] == ',')
+                        ret.Add(new Token(",", TokenType.COMMA));
+                    temp = "";
+                }
+                else temp += query[i];
+            }
+            temp = "";
+            int idx2 = -1;
+            if (idx == -1)
+            {
+                //raise excpetion
+                Console.WriteLine("hamada idx = -1");
+                return null;
+            }
+            for (int i = idx; i < query.Count(); i++)
+            {
+                if (query[i] == ' ')
+                {
+                    if (temp == "") continue;
+                    TokenType t = new Token().get_type(temp);
+                    if (t != TokenType.EXCEPT)
+                    {
+                        Token neww = new Token(temp, t);
+                        ret.Add(neww);
+                    }
+                    else
+                    {
+                        //raise exception
+                        Console.WriteLine("henanasdasd;");
+                        return null;
+                    }
+                    temp = "";
+                }
+                else
+                {
+                    temp += query[i];
+                    if (i + 4 < query.Length && query.Substring(i, 5).ToLower() == "where")
+                    {
+                        idx2 = i + 5;
+                        ret.Add(new Token("where", TokenType.WHERE));
+                        break;
+                    }
+                }
+            }
+            temp = "";
+            // select x , y+3+(5/8) , avg(x+((9*9)*(12/2))) from jaaa where bla == bla and NOT yyy=='qwe'
+            // el ahna 3mleno fe el loop el tht da ghalat 3shan el case de hy5od heta de kolha id wahd
+            for (int i = idx2; i < query.Length && i != -1; i++)
+            {
+                if (query[i] == ' ' || isoperator(query[i]) || query[i] == '(' || query[i] == ')')
+                {
+                    if (isoperator(query[i]))
+                    {
+                        string o = query[i].ToString();
+                        if (i + 1 < query.Length && isoperator(query[i + 1]))
+                        {
+                            o += query[i + 1];
+                            i++;
+                        }
+                        ret.Add(new Token(o, TokenType.OP));
+                    }
+                    else if (query[i] == '(')
+                        ret.Add(new Token("(", TokenType.L_PARA));
+                    else if (query[i] == ')')
+                        ret.Add(new Token(")", TokenType.R_PARA));
+
+                    if (temp == "") continue;
+                    TokenType t = new Token().get_type(temp);
+                    if (t != TokenType.EXCEPT)
+                    {
+                        Token neww = new Token(temp, t);
+                        ret.Add(neww);
+                    }
+                    else
+                    {
+                        //raise exception
+                        Console.WriteLine("btngahagagaga");
+                        return null;
+                    }
+                    temp = "";
+                }
+                else
+                {
+                    temp += query[i];
+                }
+            }
+
+            return ret;
+        }
+        ~lexer()
+        { }
     }
 }
--- QueryModule/QueryParser/Token.cs
+++ QueryModule/QueryParser/Token.cs
@@ -8,11 +8,49 @@ namespace QueryModule.QueryParser
 {
     enum TokenType
     {
-        SELECT, OP, ID, NUM, FROM, WHERE, L_PARA, R_PARA, COMMA, AND, OR, STRING
+        SELECT, OP, ID, NUM, FROM, WHERE, L_PARA, R_PARA, COMMA, AND, OR, STRING, EXCEPT, NOT
     }
     class Token
     {
-        TokenType tokenType;
-        string lexeme;
+        public TokenType tokenType;
+        public string lexemes;
+        public Token()
+        { }
+        public Token(string txt, TokenType type)
+        {
+            lexemes = txt;
+            tokenType = type;
+        }
+        public TokenType get_type(string s)
+        {
+            s = s.ToLower();
+            if (s == "and") return TokenType.AND;
+            if (s == "or") return TokenType.OR;
+            if (s == "not") return TokenType.NOT;
+            else if (s[0] >= 'a' && s[0] <= 'z')
+            {
+                for (int i = 0; i < s.Count(); i++)
+                {
+                    if (s[i] != '_' && (s[i] < 'a' || s[i] > 'z') && (s[i] < '0' || s[i] > '9')) return TokenType.EXCEPT;
+                }
+                return TokenType.ID;
+            }
+
+            else if (s[0] >= '0' && s[0] <= '9')
+            {
+                int cnt = 0;
+                for (int i = 0; i < s.Count(); i++)
+                {
+                    if (s[i] == '.') cnt++;
+                    if (s[i] != '.' && (s[i] < '0' || s[i] > '9')) return TokenType.EXCEPT;
+                }
+                if (cnt > 1) return TokenType.EXCEPT;
+                return TokenType.NUM;
+            }
+            else if (s.Count() > 1 && ((s[0] == '\'' && s[s.Count() - 1] == '\'') || (s[0] == '\"' && s[s.Count() - 1] == '\"')))
+                return TokenType.STRING;
+            return TokenType.EXCEPT;
+        }
     }
+
 }
